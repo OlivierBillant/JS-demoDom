c0c8cf1a74d4180fea79e7cb85b5807f8e32486e
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-10 14:16:58 +0200

    ADD : cours dom

--- demoDom.js
+++ demoDom.js
@@ -24,3 +24,27 @@ console.log(contenuParagraphe);
 let elements = document.querySelectorAll("article, p");
 console.log(elements);
 
+//Modifier le Dom
+    //Modifier le contenu d'un paragraphe
+        //On récupère le paragraphe
+        let p =  document.getElementById("monParagraphe");
+
+        // 1 - En utilisant textContent : permet d'insérer du texte dans un élément
+            p.textContent += " Message généré par Javascript avec textContent";
+            // p.textContent = "Message généré par Javascript";
+
+
+        // 2 - innerHTML : permet d'insérer du HTML dans un élément
+            p.innerHTML = "Message généré par <strong>JS</strong> avec innerHTML";
+
+    //Modifier la classe d'un element
+    p.classList.add("rouge");
+
+    //Modifier le style d'un élément
+    p.style.color = "red";
+
+    //Frabriquer de nouveaux éléments
+    let d = document.getElementById("maDiv");
+    let input = document.createElement("input");
+    input.setAttribute("type", "password");
+    d.appendChild(input);
